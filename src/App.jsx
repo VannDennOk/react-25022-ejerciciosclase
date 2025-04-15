8ccfd47a54952b97af452a85ca8cb27c06f89f32
Vanina Denegri <denegri.vanina@gmail.com>
2025-04-14 21:23:44 -0300

    Actualiza tarjetas de productos

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,65 @@ import Productos from './componentes/Productos/Index'
 import Tarjeta from './componentes/Tarjeta/Index'
 
 function App() {
-  const productos = [ 'Manzanas', 'Peras', 'Naranjas'];
+  const productos = ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4'];
 
-  
-  
-  
+  const promociones = [
+    {
+      id: 1,
+      titulo: 'Producto 1',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam…',
+      imagenUrl: 'https://i.postimg.cc/sggP0g7m/producto.png',
+      promocion: '2x1',
+      alt: 'imagen de producto'
+    },
+    {
+      id: 2,
+      titulo: 'Producto 2',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam…',
+      imagenUrl: 'https://i.postimg.cc/sggP0g7m/producto.png',
+      promocion: '-50%',
+      alt: 'imagen de producto'
+    },
+    {
+      id: 3,
+      titulo: 'Producto 3',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam…',
+      imagenUrl: 'https://i.postimg.cc/sggP0g7m/producto.png',
+      promocion: '2x1',
+      alt: 'imagen de producto'
+    },
+    {
+      id: 4,
+      titulo: 'Producto 4',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam…',
+      imagenUrl: 'https://i.postimg.cc/sggP0g7m/producto.png',
+      promocion: '-25%',
+      alt: 'imagen de producto'
+    }
+  ];
+
   return (
     <div className='container'>
       <Saludo nombre="Van"></Saludo>
-      <ListaUsuarios/>
+      <ListaUsuarios />
       <div className='container_btn'>
-        <Boton texto="Aceptar" colorBG="var(--colorVerde)"/>
-        <Boton texto="Cancelar" colorBG="var(--colorRojo)"/>
+        <Boton texto="Aceptar" colorBG="var(--colorVerde)" />
+        <Boton texto="Cancelar" colorBG="var(--colorRojo)" />
       </div>
-      <Productos productos={productos}></Productos>
-      <Tarjeta
-        titulo = "Oferta especial"
-        descripcion = "20% de descuento en todos los productos"
-        botonTexto = "Ver más"
-      ></Tarjeta>
+      <Productos productos={productos} />
+      <section className='tarjeta__container'>
+        {promociones.map((promocion) =>
+          <Tarjeta
+            key={promocion.id}
+            titulo={promocion.titulo}
+            img={promocion.imagenUrl}
+            alt={promocion.alt}
+            descripcion={promocion.descripcion}
+            promocion={promocion.promocion}
+            botonTexto="Ver más"
+          ></Tarjeta>
+        )}
+      </section>
     </div>
   )
 }
--- src/componentes/Tarjeta/Index.jsx
+++ src/componentes/Tarjeta/Index.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import "./Tarjeta.css"
 import Boton from "../Boton/Index";
 
-function Tarjeta({ titulo, descripcion, botonTexto }) {
+function Tarjeta({ titulo, img, alt, descripcion, promocion, botonTexto }) {
     
     return <div className="tarjeta">
-                <h2>{titulo}</h2>
-                <h3>{descripcion}</h3>
-                <Boton
+                <img src={img} alt={alt} />
+                <span className="tarjeta__descripcion">
+                    <h2>{titulo}</h2>
+                    <h3>{descripcion}</h3>
+                    <span className="tarjeta__promocion">
+                        <p>{promocion}</p>
+                    </span>
+                    <Boton
                     texto={botonTexto} 
                     colorBG='var(--colorAcento)'
-                />
+                    />
+                </span>
             </div>
 }
 
